c6f78f16f2b428ba394fe8a5496e44ca22ae747f
Dominik Lenz <dominik@notabot.cc>
2022-09-16 16:10:13 +0200

    adds github publish config

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,6 @@ group = "cc.notabot.noerror4j"
 dependencies {
     implementation("org.slf4j:slf4j-api:2.0.1")
     api("org.jetbrains:annotations:23.0.0")
-
 }
 
 
@@ -26,4 +25,22 @@ java {
 tasks.compileJava {
     options.javaModuleVersion.set(provider { project.version as String })
     options.compilerArgs.add("--enable-preview")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/domlen2003/no-error-4j")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 }
